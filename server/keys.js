a20c69325359d99aaca62f3c57de9dff71e42e10
Ashline Jervin <ashlinejervin@Ashlines-MacBook-Pro.local>
2025-03-26 08:13:44 +0530

    corrected file

--- server/keys.js
+++ server/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT
+  pgUser: process.env.POSTGRES_USER,
+  pgHost: process.env.POSTGRES_HOST,
+  pgDatabase: process.env.POSTGRES_DB,
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgPort: process.env.POSTGRES_PORT
 };
